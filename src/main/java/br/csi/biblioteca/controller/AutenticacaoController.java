bfdb6a75d589addffc808da9ae628643c25fca26
Nicole Grazzioli <nicolegrazzioli@gmail.com>
2025-11-29 18:46:46 -0300

    add verificacao de usuario inativo

--- src/main/java/br/csi/biblioteca/controller/AutenticacaoController.java
+++ src/main/java/br/csi/biblioteca/controller/AutenticacaoController.java
@@ -22,74 +22,78 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import java.util.Map;
 
-
 record DadosAutenticacao(
-        @Schema(description = "Email do usuário", example = "admin@email.com") String login,
-        @Schema(description = "Senha do usuário", example = "admin123") String senha
-) {}
+                @Schema(description = "Email do usuário", example = "admin@email.com") String login,
+                @Schema(description = "Senha do usuário", example = "admin123") String senha) {
+}
+
 record DadosToken(
-        @Schema(description = "Token JWT de autenticação", example = "eyJhbGciOiJIUzI1NiIsInR5cCI...") String token
-) {}
+                @Schema(description = "Token JWT de autenticação", example = "eyJhbGciOiJIUzI1NiIsInR5cCI...") String token) {
+}
 
 @RestController
 @RequestMapping("/login")
 @AllArgsConstructor
 @Tag(name = "Login", description = "Operações de login: autenticação de usuários e geração de token JWT")
 public class AutenticacaoController {
-    private final AuthenticationManager manager;
-    private final TokenServiceJWT tokenService;
-    private static final Logger logger = LoggerFactory.getLogger(AutenticacaoController.class);
+        private final AuthenticationManager manager;
+        private final TokenServiceJWT tokenService;
+        private static final Logger logger = LoggerFactory.getLogger(AutenticacaoController.class);
 
+        @Operation(summary = "Autenticação de usuário", description = "Autentica um usuário com email e senha e retorna um token JWT") // descrição
+                                                                                                                                       // do
+                                                                                                                                       // endpoint
+        @ApiResponses(value = { // respostas possiveis que o endpoint pode retornar + descrição
+                        @ApiResponse(responseCode = "200", description = "Login bem-sucedido - retorna o token JWT", content = @Content(mediaType = "application/json", schema = @Schema(implementation = DadosToken.class))),
+                        @ApiResponse(responseCode = "401", description = "Credenciais inválidas: usuário ou senha incorretos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDTO.class), examples = @ExampleObject(value = "{\"mensagem\": \"Usuário ou senha incorretos\"}"))),
+                        @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDTO.class), examples = @ExampleObject(value = "{\"mensagem\": \"Erro interno do servidor\"}")))
+        })
+        @PostMapping
+        public ResponseEntity<?> login(@RequestBody DadosAutenticacao dados) {
+                System.out.println("--- TENTATIVA DE LOGIN VIA FRONT ---");
+                System.out.println("Objeto recebido: " + dados);
+                System.out.println("Login recebido: '" + dados.login() + "'");
+                System.out.println("Senha recebida: '" + dados.senha() + "'");
+                System.out.println("------------------------------------");
 
-    @Operation(summary = "Autenticação de usuário", description = "Autentica um usuário com email e senha e retorna um token JWT") //descrição do endpoint
-    @ApiResponses(value = { //respostas possiveis que o endpoint pode retornar + descrição
-            @ApiResponse(responseCode = "200", description = "Login bem-sucedido - retorna o token JWT",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = DadosToken.class))),
-            @ApiResponse(responseCode = "401", description = "Credenciais inválidas: usuário ou senha incorretos",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ApiErrorDTO.class),
-                            examples = @ExampleObject(value = "{\"mensagem\": \"Usuário ou senha incorretos\"}")
-                    )),
-            @ApiResponse(responseCode = "500", description = "Erro interno no servidor",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ApiErrorDTO.class),
-                            examples = @ExampleObject(value = "{\"mensagem\": \"Erro interno do servidor\"}")))
-    })
-    @PostMapping
-    public ResponseEntity<?> login(@RequestBody DadosAutenticacao dados) {
-        System.out.println("--- TENTATIVA DE LOGIN VIA FRONT ---");
-        System.out.println("Objeto recebido: " + dados);
-        System.out.println("Login recebido: '" + dados.login() + "'");
-        System.out.println("Senha recebida: '" + dados.senha() + "'");
-        System.out.println("------------------------------------");
+                try {
 
-        try {
+                        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+                        Authentication authentication = manager.authenticate(authenticationToken);
 
-            var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-            Authentication authentication = manager.authenticate(authenticationToken);
+                        // Se a autenticação for bem-sucedida, o Spring retorna o objeto UserDetails
+                        // (Usuario)
+                        var usuario = (Usuario) authentication.getPrincipal();
+                        String token = tokenService.gerarToken(usuario);
 
-            // Se a autenticação for bem-sucedida, o Spring retorna o objeto UserDetails (Usuario)
-            var usuario = (Usuario) authentication.getPrincipal();
-            String token = tokenService.gerarToken(usuario);
+                        // return ResponseEntity.ok(token);
+                        return ResponseEntity.ok().body(new DadosToken(token));
+                } catch (DisabledException e) {
+                        // dados corretos mas usuário inativo (ativoUs = false) - 403 Forbidden
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Usuário inativo");
 
-//            return ResponseEntity.ok(token);
-            return ResponseEntity.ok().body(new DadosToken(token));
-        } catch (AuthenticationException e) {
-            //usuario ou senha invalidos = 401
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha incorretos");
-        } catch (Exception e) {
-            //e.printStackTrace();
-            logger.error("Erro interno ao processar login: {}", e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno de processamento do login"); //500
+                } catch (BadCredentialsException e) {
+                        // dados incorretos - 401 Unauthorized
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha incorretos");
+
+                } catch (AuthenticationException e) {
+                        // outros erros de autenticação - 401 Unauthorized
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Erro de autenticação");
+                } catch (Exception e) {
+                        // e.printStackTrace();
+                        logger.error("Erro interno ao processar login: {}", e.getMessage(), e);
+                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                        .body("Erro interno de processamento do login"); // 500
+                }
+
         }
 
-    }
-
-//    private record DadosAutenticacao(String login, String senha) {}
-//    private record DadosToken(String token) {}
+        // private record DadosAutenticacao(String login, String senha) {}
+        // private record DadosToken(String token) {}
 
 }
